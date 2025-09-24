25cad2b23e90b57fdca3acfa284b743c3d5993c7
Ryan0730917 <lij71610@gmail.com>
2025-09-24 11:39:41 +0800

    week03

---
+++ week03-3b.c
@@ -0,0 +1,17 @@
+//week03-3b.cpp 二合一
+//Leetcode 學習計畫
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int ans =1; //1乘上任何數,就突然變成(任何數)
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]>0)ans*= +1;
+            if(nums[i]<0)ans*= -1;
+            if(nums[i]==0)ans*= 0;
+        }
+        if(ans>0)return 1;
+        if(ans<0)return -1;
+        return 0;
+
+    }
+};
