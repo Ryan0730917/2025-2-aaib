b94ff26ca18a49f1689d463143f93678d48fdc61
Ryan0730917 <lij71610@gmail.com>
2025-11-05 11:54:22 +0800

    week09

---
+++ week09/week 09-2.cpp
@@ -0,0 +1,31 @@
+//week 09-2.cpp
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int M = matrix.size(), N = matrix[0].size();
+        vector<bool> markI(M), markJ(N);
+        for (int i=0; i<M; i++){
+            for (int j=0; j<N; j++){
+                if (matrix[i][j]==0){
+                    markI[i] = true;
+                    markJ[j] = true;
+                }
+            }
+        }
+        for(int i=0; i<M; i++) cout << markI[i] << " ";
+        cout << "\n";
+        for(int j=0; j<N; j++) cout << markJ[j] << " ";
+        cout << "\n";
+
+        for(int i=0; i<M; i++){
+            for (int j=0; j<N; j++){
+                if (markI[i]==true || markJ[j]==true) {
+                    cout << "0";
+                    matrix[i][j] = 0;
+                }else cout << "x";
+                cout << matrix[i][j] << " ";
+            }
+            cout << "\n";
+        }
+    }
+};
